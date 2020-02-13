d93da4af892147b40d3f87bcf0256acf4bbaf9c8
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-13 06:06:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
