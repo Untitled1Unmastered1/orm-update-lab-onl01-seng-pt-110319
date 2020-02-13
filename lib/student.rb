f7552ef317c245631edfa4aeece19f3f56d999fd
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-13 06:09:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,5 +44,9 @@ end
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+  
+  def self.create 
+    sql = <<-SQL 
+    
 
 end
